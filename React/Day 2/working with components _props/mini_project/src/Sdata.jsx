0342b66c8160b16fda26f65044e2446e52e1e758
Suraj Sawant <53471115+surajsawant211@users.noreply.github.com>
2021-10-04 02:03:01 +0530

    Add files via upload

---
+++ React/Day 2/working with components _props/mini_project/src/Sdata.jsx
@@ -0,0 +1,46 @@
+const Sdata =[
+    {
+        id:1,
+        imgsrc:'https://www.wallpapertip.com/wmimgs/162-1622116_a-r-rahman-wallpaper-ar-rahman-photos-hd.jpg',
+        title:'Bollywood Music Directors',
+        name:'A R Rehman',
+        link:'https://youtu.be/zkQQDMuglcE',
+    },
+
+    {
+        id:2,
+        imgsrc:"https://static.toiimg.com/thumb/msid-78109420,imgsize-883781,width-800,height-600,resizemode-75/78109420.jpg",
+        title:"Bollywood Music Directors",
+        name:"Ajay-Atul",
+        link:"https://www.youtube.com/watch?v=ljCJ01aSctY",
+    },
+
+    {
+        id:3, 
+        imgsrc:"http://media.santabanta.com/newsite/cinemascope/feed/vishal-shekhar.jpg",
+        title:"Bollywood Music Directors",
+        name:"Vishal-Shekhar",
+        link:"https://youtu.be/w9vUEcE3E8Y",
+    },
+
+    {
+        id:4,
+        imgsrc:"https://lh6.googleusercontent.com/-cDH62Ee75M4/U1CbimMDGMI/AAAAAAAAFoo/vZIXmrwpnDo/s585/Sajid-Wajid-Hot-Scene-Shirtless-Body-Image.jpg",
+        title:"Bollywood Music Directors",
+        name:"Sajid-Wajid",
+        link:"https://www.youtube.com/watch?v=5ETS2iiwH-c",
+    },
+
+    {
+        id:5,
+        imgsrc:"https://www.teahub.io/photos/full/212-2120350_pritam-chakraborty-date-of-birth-pritam-performing.jpg",
+        title:"Bollywood Music Directors",
+        name:"Pritam Chakraborty",
+        link:"https://youtu.be/S3pLJi7PN5E",
+    },
+
+    
+
+]
+
+export default Sdata
