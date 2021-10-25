f76406c2d67d627009c9aeec0cc7fc7c0a1a4348
Suraj Sawant <53471115+surajsawant211@users.noreply.github.com>
2021-10-25 15:18:32 +0530

    Add files via upload

---
+++ React/Day 9/reduxapp/src/Reducers/index.js
@@ -0,0 +1,11 @@
+import changeTheNumber from './upDown'
+
+import { combineReducers } from 'redux'
+
+const rootReducers = combineReducers(
+    {
+        changeTheNumber
+    }
+);
+
+export default rootReducers;
