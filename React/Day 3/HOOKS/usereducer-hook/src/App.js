85cdbc9a93f2e1ecbb36cc70092f547c19cb6515
Suraj Sawant <53471115+surajsawant211@users.noreply.github.com>
2021-10-25 14:47:03 +0530

    Add files via upload

---
+++ React/Day 3/HOOKS/usereducer-hook/src/App.js
@@ -0,0 +1,12 @@
+import UseReducer from './hook/UseReducer';
+import './App.css'
+
+function App() {
+  return (
+    <div >
+      <UseReducer />
+    </div>
+  );
+}
+
+export default App;
---
+++ React/Day 3/HOOKS/usereducer-hook/src/hook/UseReducer.js
@@ -0,0 +1,35 @@
+import React,{useReducer, useState} from 'react'
+import './usereducers.css'
+
+const initialState=0
+const reducer=(state,action)=>{
+    console.log(state,action)
+    if(action.type === 'INCREMENT'){
+        return state + 1
+    }
+    if(action.type === 'DECREMENT'){
+        return state - 1
+    }
+    return state
+
+}
+
+const UseReducer = () => {
+
+    const [state, dispatch]= useReducer(reducer,initialState)
+
+    // const [count,setCount]=useState(0)
+    return (
+        <>
+            <div>
+                <p> {state} </p>
+            </div>
+            <div className='btnStylePosition'>
+                <button onClick={() => dispatch({type: 'INCREMENT'})}>Inc</button>
+                <button onClick={() => dispatch({type: 'DECREMENT'})}>Dec</button>
+            </div>
+        </>
+    )
+}
+
+export default UseReducer
