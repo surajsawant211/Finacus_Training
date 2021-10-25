bcc003ddd31aac2ef6ca2e9357eec7dc4df2a298
Suraj Sawant <53471115+surajsawant211@users.noreply.github.com>
2021-10-25 14:54:14 +0530

    Add files via upload

---
+++ React/Day 5/todo-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import ToDo from './component/todo';
+
+function App() {
+  return (
+    <div>
+      <ToDo />
+    </div>
+  );
+}
+
+export default App;
